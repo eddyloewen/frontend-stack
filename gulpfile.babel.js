420ef9ccb40cbe28747ca4d20ae0ca3e68cb81c9
Eddy Löwen <mail@eddyloewen.com>
2020-05-07 09:54:29 +0200

    uses new prettier and editorconfig styles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,24 +6,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const files = {
-    clean: ['public/assets/*', 'public/css/*', 'public/js/*'],
-    copy: [
-        {
-            src: 'src/assets/images/*.*',
-            dest: 'public/assets/images',
-        },
-    ],
-    svg: [
-        {
-            src: 'src/assets/icons/*.svg',
-            dest: 'public/assets/icons',
-        },
-    ],
+	clean: ['public/assets/*', 'public/css/*', 'public/js/*'],
+	copy: [
+		{
+			src: 'src/assets/images/*.*',
+			dest: 'public/assets/images',
+		},
+	],
+	svg: [
+		{
+			src: 'src/assets/icons/*.svg',
+			dest: 'public/assets/icons',
+		},
+	],
 };
 
 options({
-    projectTitle: 'Frontend Stack',
-    versionManifest: { name: 'public/hash-manifest.json' },
+	projectTitle: 'Frontend Stack',
+	versionManifest: { name: 'public/hash-manifest.json' },
 });
 
 task('clean', tasks.clean({ directories: files.clean }));
@@ -39,28 +39,28 @@ task('lintJS', tasks.lintJS({ src: 'src/js/*.js' }));
 task('ls', tasks.run({ cmd: 'ls', args: ['-la'] }));
 
 task(
-    'watch',
-    tasks.watch({
-        paths: [
-            {
-                src: ['src/css/**/*.css', 'tailwind.config.js'],
-                tasks: series('lintCSS', 'css'),
-                changeMessage: 'CSS changed',
-            },
-            {
-                src: ['src/assets/**/*.svg'],
-                tasks: series('svg'),
-                changeMessage: 'SVG Sprite changed',
-            },
-        ],
-    }),
+	'watch',
+	tasks.watch({
+		paths: [
+			{
+				src: ['src/css/**/*.css', 'tailwind.config.js'],
+				tasks: series('lintCSS', 'css'),
+				changeMessage: 'CSS changed',
+			},
+			{
+				src: ['src/assets/**/*.svg'],
+				tasks: series('svg'),
+				changeMessage: 'SVG Sprite changed',
+			},
+		],
+	}),
 );
 
 task(
-    'browserSync',
-    tasks.browserSync({
-        files: ['public/**/*.js', 'public/**/*.css'],
-    }),
+	'browserSync',
+	tasks.browserSync({
+		files: ['public/**/*.js', 'public/**/*.css'],
+	}),
 );
 
 task('default', series('clean', parallel('copy', 'svg', 'css', 'js')));
@@ -69,15 +69,15 @@ task('dev:watch', series('dev', 'watch'));
 task('dev:sync', parallel('dev:watch', 'browserSync'));
 task('prod', series('default'));
 
-const log = filesGlob => {
-    return () => {
-        return new Promise(resolve => {
-            glob(filesGlob, function (error, files) {
-                console.log('custom log task', files);
-            });
-            resolve();
-        });
-    };
+const log = (filesGlob) => {
+	return () => {
+		return new Promise((resolve) => {
+			glob(filesGlob, function (error, files) {
+				console.log('custom log task', files);
+			});
+			resolve();
+		});
+	};
 };
 
 task('log', log('src/css/*.css'));
